ae6bf963e2b85b8adbfb0c28b473103d2fc5768a
Damilare Joseph <dre4success@live.com>
2017-08-13 14:26:40 +0100

    saved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
+	res.send({ welcome: 'yes thank you' });
 });
 
 const PORT = process.env.PORT || 5000;
